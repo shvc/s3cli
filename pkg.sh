04a4b9125c173238a6c9b91141f1676d47159f18
awegcc <dawter@qq.com>
2019-06-16 15:56:16 +0800

    refine build script

--- pkg.sh
+++ pkg.sh
@@ -9,14 +9,14 @@ then
   endpoint=$1
 fi
 
-echo "Building Linux amd64 ..."
+echo "Building Linux amd64 s3cli-$version"
 GOOS=linux GOARCH=amd64 go build -ldflags " -X main.version=$version -X main.endpoint=$endpoint"
 zip -m s3cli-$version-linux-amd64.zip s3cli
 
-echo "Building Macos amd64 ..."
+echo "Building Macos amd64 s3cli-$version"
 GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.version=$version -X main.endpoint=$endpoint"
 zip -m s3cli-$version-macos-amd64.zip s3cli
 
-echo "Building Windows amd64 ..."
+echo "Building Windows amd64 s3cli-$version"
 GOOS=windows GOARCH=amd64 go build -ldflags " -X main.version=$version -X main.endpoint=$endpoint"
 zip -m s3cli-$version-win-x64.zip s3cli.exe
