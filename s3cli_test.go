08a080d97aae1465ddc17868e99245784f3b873a
vager <chencheng@myshare.io>
2019-07-09 21:54:38 +0800

    add testcode

--- s3cli_test.go
+++ s3cli_test.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"os"
 	"testing"
 )
 
@@ -13,14 +13,28 @@ import (
 //	AWS_SECRET_ACCESS_KEY=SECRET
 //	AWS_SECRET_KEY=SECRET # only read if AWS_SECRET_ACCESS_KEY is not set.
 
-var s3cli = S3Cli{}
+var s3cli = S3Cli{
+	endpoint: "https://play.min.io:9000",
+	region:   "default",
+}
 
+func setup() error {
+	err := os.Setenv("AWS_ACCESS_KEY", "Q3AM3UQ867SPQQA43P2F")
+	if err != nil {
+		return err
+	}
+	return os.Setenv("AWS_SECRET_KEY", "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG")
+}
 func Test_loadS3Cfg(t *testing.T) {
+	err := setup()
+	if err != nil {
+		t.Errorf("setup failed: %s", err)
+	}
 	cfg, err := s3cli.loadS3Cfg()
 	if err != nil {
 		t.Errorf("loadS3config failed")
 	}
-	fmt.Println(cfg)
+	t.Log(cfg)
 }
 
 func Test_newS3Client(t *testing.T) {
@@ -44,7 +58,14 @@ func Test_deleteBucket(t *testing.T) {
 }
 
 func Test_listBuckets(t *testing.T) {
-	// TODO
+	err := setup()
+	if err != nil {
+		t.Errorf("setup failed: %s", err)
+	}
+	err = s3cli.listBuckets()
+	if err != nil {
+		t.Errorf("listBuckets failed: %s", err)
+	}
 }
 
 func Test_aclBucket(t *testing.T) {
